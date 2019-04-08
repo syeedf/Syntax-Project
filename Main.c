8ee1f1b9ccabd4217237ed0fe5e51c581a9d23cb
syeedf <46726260+syeedf@users.noreply.github.com>
2019-04-08 17:08:25 -0400

    Copying program from the textbook.

--- Main.c
+++ Main.c
@@ -1,4 +1,10 @@
 #include <stdio.h> 
+#include <ctype.h>
+/* Global declarations */ /* Variables */ 
+int  charClass; 
+char  lexeme[100]; 
+char  nextChar; 
+int  lexLen;
 int main(void) {
 	printf("Hello World\n");
 	return 0;
