1ad2fc0849b01d21266f8a35b481e4ec72216894
syeedf <46726260+syeedf@users.noreply.github.com>
2019-04-08 17:23:24 -0400

    Initializing integers for later use in the program.

--- Main.c
+++ Main.c
@@ -5,6 +5,11 @@ int  charClass;
 char  lexeme[100]; 
 char  nextChar; 
 int  lexLen;
+int  token; 
+int  nextToken; 
+FILE *in_fp, *fopen();
+/* Function declarations */ 
+void  addChar();
 int main(void) {
 	printf("Hello World\n");
 	return 0;
